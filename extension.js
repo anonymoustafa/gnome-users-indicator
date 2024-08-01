1a1d71f8726195e80b12e7718e44c432a0b82dd7
Mostafa Ramezani <anonymoustafa@outlook.com>
2024-08-01 16:06:19 -0400

    hello

--- extension.js
+++ extension.js
@@ -1,45 +1,40 @@
+import GObject from 'gi://GObject';
 import St from 'gi://St';
 
-import {Extension, gettext as _, ngettext, pgettext} from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
+const Indicator = GObject.registerClass(
+class Indicator extends PanelMenu.Button {
+    _init() {
+        super._init(0.0, _('Hello World Indicator'));
 
-export default class ExampleExtension extends Extension {
-    enable() {
-        // Create a panel button
-        this._indicator = new PanelMenu.Button(5.1, this.metadata.name, false);
-
-        // Add an icon
-        const icon = new St.Icon({
-            icon_name: 'face-laugh-symbolic',
+        // Replace the icon with a text label
+        this.add_child(new St.Label({
+            text: 'Hello World',
             style_class: 'system-status-icon',
-        });
-        this._indicator.add_child(icon);
+        }));
 
-        // Add the indicator to the panel
+        let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
+        item.connect('activate', () => {
+            Main.notify(_('Whatʼs up, folks?'));
+        });
+        this.menu.addMenuItem(item);
+    }
+});
+
+// Export the extension for GNOME Shell
+export default class MyExtension extends Extension {
+    enable() {
+        this._indicator = new Indicator();
         Main.panel.addToStatusArea(this.uuid, this._indicator);
-
-        // A string needing more context is marked with `pgettext()`
-        this._indicator.menu.addAction(pgettext('menu item', 'Notify'), () => {
-            this._count += 1;
-
-            // A regular translatable string is marked with the `_()` function
-            const title = _('Notification');
-
-            // A "countable" string is marked with the `ngettext()` function
-            const body = ngettext('You have been notified %d time',
-                'You have been notified %d times',
-                this._count).format(this._count);
-
-            Main.notify(title, body);
-        });
-
-        this._count = 0;
     }
 
     disable() {
-        this._indicator?.destroy();
+        this._indicator.destroy();
         this._indicator = null;
     }
 }
+
