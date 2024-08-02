9fce051e1482a8d3a45092e744ca41861061cee4
Mostafa Ramezani <anonymoustafa@outlook.com>
2024-08-02 11:56:08 -0400

    hello

--- extension.js
+++ extension.js
@@ -1,24 +1,41 @@
-const St = imports.gi.St;
-const Main = imports.ui.main;
-const Mainloop = imports.mainloop;
+import GObject from 'gi://GObject';
+import St from 'gi://St';
 
-let panelButton;
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-function init() {
+const Indicator = GObject.registerClass(
+class Indicator extends PanelMenu.Button {
+    _init() {
+        super._init(0.0, _('Hello World Indicator'));
+
+        // Replace the icon with a text label
+        this.add_child(new St.Label({
+            text: 'Hello World',
+            style_class: 'system-status-icon',
+        }));
+
+        let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
+        item.connect('activate', () => {
+            Main.notify(_('Whatʼs up, folks?'));
+        });
+        this.menu.addMenuItem(item);
+    }
+});
+
+// Export the extension for GNOME Shell
+export default class MyExtension extends Extension {
+    enable() {
+        this._indicator = new Indicator();
+        Main.panel.addToStatusArea(this.uuid, this._indicator);
+    }
+
+    disable() {
+        this._indicator.destroy();
+        this._indicator = null;
+    }
 }
 
-function enable() {
-    panelButton = new St.Bin({
-        style_class: 'panel-button'
-    });
-    let label = new St.Label({
-        text: "Hello, World!",
-        y_align: St.Align.MIDDLE
-    });
-    panelButton.set_child(label);
-    Main.panel._rightBox.insert_child_at_index(panelButton, 0);
-}
 
-function disable() {
-    Main.panel._rightBox.remove_child(panelButton);
-}
